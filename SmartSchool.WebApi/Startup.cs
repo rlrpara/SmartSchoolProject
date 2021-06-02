b1f9818cdbc5b93b9587e57bf3b3b69115295d6f
rlrpara <rlr.para@gmail.com>
2021-06-02 20:12:15 -0300

    ...

--- SmartSchool.WebApi/Startup.cs
+++ SmartSchool.WebApi/Startup.cs
@@ -16,7 +16,7 @@ namespace SmartSchool.WebApi
 {
     public class Startup
     {
-        private readonly string _baseUrl = "http://www.rlrsistemas.com.br";
+        private readonly string _baseUrl = Environment.GetEnvironmentVariable("BASE_URL");
         public IConfiguration Configuration { get; }
 
         public Startup(IConfiguration configuration)
